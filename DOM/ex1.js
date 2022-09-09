9afeccf7cea9f8f5a7e6c3d7d43abcd4649ef18e
GUILHERME PEREIRA DEZENA <guilherme.pdezena@senacsp.edu.br>
2022-09-09 20:49:53 -0300

    Iniciando DOM

--- DOM/ex1.js
+++ DOM/ex1.js
@@ -1,1 +1,6 @@
-console.log(window)
+//alert('Sua resolução é ' + window.innerHeight + "x" + window.innerWidth)
+let meuTitulo = document.getElementById('nome')
+
+meuTitulo.innerText = 'teste';
+meuTitulo.style.background = '#dedede'
+meuTitulo.style.color = 'red'
