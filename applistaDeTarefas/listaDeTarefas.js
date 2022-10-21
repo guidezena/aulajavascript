2f465a32b2f860d48f0ba9f4edd759c5e7216e33
GUILHERME PEREIRA DEZENA <guilherme.pdezena@senacsp.edu.br>
2022-10-21 20:21:56 -0300

    Usando evento keypress

--- applistaDeTarefas/listaDeTarefas.js
+++ applistaDeTarefas/listaDeTarefas.js
@@ -4,7 +4,7 @@ const listaDeTarefas = document.querySelector('.collection');
 const filtroDeTarefa = document.querySelector('#filtro');
 const btnLimpaTudo = document.querySelector('.limpar-tarefas');
 
-function carregaMonitoresDeEventos(){
+function carregaMonitoresDeEventos() {
 
     //evento para adicionar a tarefa
     btnAddTarefa.addEventListener('click', adicionarTarefa);
@@ -13,12 +13,12 @@ function carregaMonitoresDeEventos(){
 }
 carregaMonitoresDeEventos();
 
-function apagarTarefa(evento){
+function apagarTarefa(evento) {
 
     //se o elemento pai tiver a classe apaga-tarefa,
     //ou seja, for o elemento "a", apague li, ou seja, 
     //a tarefa
-    if( evento.target.parentElement.classList.contains('apaga-tarefa') ){
+    if (evento.target.parentElement.classList.contains('apaga-tarefa')) {
 
         //apaga o elemento pai do "a", ou seja, o li, pois
         //o elemento alvo do evento foi o i 
@@ -26,12 +26,17 @@ function apagarTarefa(evento){
     }
 }
 
-function adicionarTarefa(evento){
+function filtrarTarefa(evento){
+    document.addEventListener('keypress', (evento) => )
+}
+
+function adicionarTarefa(evento) {
     evento.preventDefault();
 
     //verifica se usuário entrou com uma tarefa
-    if( entradaTarefa.value === '' ){
+    if (entradaTarefa.value === '') {
         alert('Entre com uma tarefa');
+        return false
     }
 
     //cria li com a nova tarefa
