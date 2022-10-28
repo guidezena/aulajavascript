5c17f58582042f3ca027ee1ba4a6d25e48cbc64e
GUILHERME PEREIRA DEZENA <guilherme.pdezena@senacsp.edu.br>
2022-10-28 20:42:54 -0300

    limpando elementos localstorge

--- applistaDeTarefas/listaDeTarefas.js
+++ applistaDeTarefas/listaDeTarefas.js
@@ -4,7 +4,7 @@ const listaDeTarefas = document.querySelector('.collection');
 const filtroDeTarefa = document.querySelector('#filtro');
 const btnLimpaTudo = document.querySelector('.limpar-tarefas');
 
-function carregaMonitoresDeEventos() {
+function carregaMonitoresDeEventos(){
 
     //evento para adicionar a tarefa
     btnAddTarefa.addEventListener('click', adicionarTarefa);
@@ -18,7 +18,7 @@ function carregaMonitoresDeEventos() {
 }
 carregaMonitoresDeEventos();
 
-function filtrar(evento) {
+function filtrar(evento){
 
     //capturamos o que o usuário está digitando
     //passando tudo para minúsculo
@@ -28,7 +28,7 @@ function filtrar(evento) {
 
     //Para cada tarefa existente, busque a string
     //desejada
-    tarefas.forEach(function (tarefa) {
+    tarefas.forEach(function(tarefa){
 
         //Recuperamos apenas o texto do elemento 
         //<li> onde está a tarefa
@@ -36,29 +36,42 @@ function filtrar(evento) {
 
         //Procuramos a string digitada no filtro
         //no texto que está no <li>
-        if (textoTarefa.toLowerCase().indexOf(procurado) != -1) {
+        if( textoTarefa.toLowerCase().indexOf(procurado) != -1 ){
 
             tarefa.style.display = 'block';
-        } else {
+        }else{
 
             tarefa.style.display = 'none';
         }
     });
 }
 
-function apagarTudo(evento) {
+function apagarTudo(evento){
     evento.preventDefault();
-
-    listaDeTarefas.innerHTML = '';
+    
+    listaDeTarefas.innerHTML = '';    
 }
 
-function apagarTarefa(evento) {
+function apagarTarefa(evento){
 
     //se o elemento pai tiver a classe apaga-tarefa,
+    //ou seja, for o elemento "a", apague li, ou seja, 
+    //a tarefa
+    if( evento.target.parentElement.classList.contains('apaga-tarefa') ){
+
+        //apaga o elemento pai do "a", ou seja, o li, pois
+        //o elemento alvo do evento foi o i 
+        evento.target.parentElement.parentElement.remove();
+    }
+}
+
+function adicionarTarefa(evento){
+    evento.preventDefault();
+
     //verifica se usuário entrou com uma tarefa
-    if (entradaTarefa.value === '') {
+    if( entradaTarefa.value === '' ){
         alert('Entre com uma tarefa');
-        return false
+        return false;
     }
 
     //cria li com a nova tarefa
@@ -86,13 +99,13 @@ function apagarTarefa(evento) {
     entradaTarefa.value = '';
 }
 
-function gravarTarefa(tarefa) {
+function gravarTarefa(tarefa){
 
     let tarefas = [];
 
     let tarefaDoStorage = localStorage.getItem('tarefas');
 
-    if (tarefaDoStorage != null) {
+    if( tarefaDoStorage != null ){
 
         tarefas = JSON.parse(tarefaDoStorage);
     }
@@ -101,3 +114,6 @@ function gravarTarefa(tarefa) {
 
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
 }
+
+
+
