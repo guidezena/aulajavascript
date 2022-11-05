a8a235b858dabccd9810b5b6206190b65746fed9
GUILHERME PEREIRA DEZENA <guilherme.pdezena@senacsp.edu.br>
2022-11-04 21:29:08 -0300

    arrumando pastas dos exercicios

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,36 +1,21 @@
 document.
     querySelector('button').
-    addEventListener('click', recuperaConteudoAjax);
+        addEventListener('click', recuperaConteudoAjax);
 
-function recuperaConteudoAjax() {
+function recuperaConteudoAjax(){
 
     const XHR = new XMLHttpRequest;
 
     XHR.open('GET', 'http://127.0.0.1:5500/ajax/conteudo.txt', true);
 
-    XHR.onload = function () {
+    XHR.onload = function(){
 
-        if (this.status == 200) {
+        if(this.status == 200){
 
-            let data = JSON.parse(this.responseText)
-            console.log(data)
-            //document.querySelector('#dadoRecuperado').innerHTML
-              //  = this.responseText;
-
-            let user = prompt('Qual o seu nome?')
-            data.forEach(function(valor){
-                if(valor.nome == user && valor.idade < 18){
-                    alert("Usuario menor de idade")
-                }else if(valor.nome !== user){
-                    alert('Não existe esse usuario')
-                }else{
-                    alert('Usuario maior de idade')
-                }
-            });
+            document.querySelector('#dadoRecuperado').innerHTML 
+                = this.responseText; 
         }
     }
 
     XHR.send();
-
-
 }        
---
+++ ajax/ex1.js
@@ -0,0 +1,48 @@
+document.
+    querySelector('button').
+    addEventListener('click', recuperaConteudoAjax);
+
+function recuperaConteudoAjax() {
+
+    const XHR = new XMLHttpRequest;
+
+    XHR.open('GET', 'http://127.0.0.1:5500/ajax/conteudo.txt', true);
+
+    XHR.onload = function () {
+
+        if (this.status == 200) {
+
+            let data = JSON.parse(this.responseText)
+            
+                console.log(data)
+            //document.querySelector('#dadoRecuperado').innerHTML
+            //  = this.responseText;
+
+            let saida = ""
+            data.array.forEach(function(aluno) {
+                saida += aluno.idade >= 18 ?
+                    `${aluno.nome} é maior de idade <br> ` :
+                    `${aluno.nome} é menor de idade <br> `
+
+            });
+            console.log(data)
+            document.querySelector('#dadoRecuperado').innerHTML = saida
+
+            /*let user = prompt('Qual o seu nome?')
+            data.forEach(function (element) {
+                if (element.nome == user && element.idade < 18) {
+                    alert("Usuario menor de idade")
+                } else if (element.nome !== user) {
+                    alert('Não existe esse usuario')
+                } else {
+                    alert('Usuario maior de idade')
+                }
+
+            });*/
+        }
+    }
+
+    XHR.send();
+
+
+}        
